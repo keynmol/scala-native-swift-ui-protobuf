b17303dbabe86f4baddb6a321b6cf965ab4f3e37
Anton Sviridov <keynmol@gmail.com>
2023-10-29 13:22:07 +0000

    Initial Commit

---
+++ Scala-Native-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scala-Native-SwiftUI
+//
+//  Created by Anton Sviridov on 29/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scala-Native-SwiftUI/Scala_Native_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scala_Native_SwiftUIApp.swift
+//  Scala-Native-SwiftUI
+//
+//  Created by Anton Sviridov on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Scala_Native_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
