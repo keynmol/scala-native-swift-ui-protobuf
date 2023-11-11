02476a78c37588f3fc0b5c539877f6558eb9d7f8
Anton Sviridov <keynmol@gmail.com>
2023-11-11 10:05:21 +0000

    WIP

--- Scala-Native-SwiftUI/scala-code/project.scala
+++
@@ -1,7 +0,0 @@
-//> using platform scala-native
-//> using dep com.thesamet.scalapb::scalapb-runtime::0.11.14
-//> using dep com.outr::scribe::3.12.2
-//> using dep com.indoorvivants.twom8::twotm8-client::dev
-//> using scala 3.3.1
-//> using option -Wunused:all
-
